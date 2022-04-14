75a7afc4bee46b35c31af4f1351ec6583143f951
Luis-Dev <luispaixao@id.uff.br>
2022-04-13 21:57:05 -0300

    adição classes Boleto e Fatura

---
+++ demo/src/main/java/com/example/Boleto.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.util.Date;
+
+public class Boleto {
+    private int codigo;
+    private Date data;
+    private float valor;
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public float getValor() {
+        return valor;
+    }
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    public Date getData() {
+        return data;
+    }
+    public void setData(Date data) {
+        this.data = data;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    
+    
+    
+}
---
+++ demo/src/main/java/com/example/Fatura.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.Date;
+
+public class Fatura {
+    private Date Data;
+    private float valorTotal;
+    private String Cliente;
+    
+    public Date getData() {
+        return Data;
+    }
+    public String getCliente() {
+        return Cliente;
+    }
+    public void setCliente(String cliente) {
+        this.Cliente = cliente;
+    }
+    public float getValorTotal() {
+        return valorTotal;
+    }
+    public void setValorTotal(float valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    public void setData(Date data) {
+        this.Data = data;
+    }
+    
+}
